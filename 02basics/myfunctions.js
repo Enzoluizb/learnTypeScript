3b3437abe4cffd62f6f5a51b768a8ff3de95f78a
enzoluizb <enzoluiz.brt@gmail.com>
2024-12-22 16:10:34 -0300

    tsc myfunction.ts

--- 02basics/myfunctions.js
+++ 02basics/myfunctions.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
-    // return num + 2
-    return "hello";
+    return num + 2;
+    // return "hello"
 }
 function getUpper(val) {
     return val.toUpperCase();
@@ -15,3 +15,24 @@ var myValue = addTwo(5);
 getUpper("string");
 signUpUser("name", "name@email.com", false);
 loginUser("n", "n@n.com");
+// function getValue(myVal: number) {
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3]
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+    // return 1
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
