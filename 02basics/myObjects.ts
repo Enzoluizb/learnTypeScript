3f86ddf78a03201c89313d0323793af158c43afd
enzoluizb <enzoluiz.brt@gmail.com>
2024-12-23 10:26:41 -0300

    type aliases

--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -1,19 +1,31 @@
-const User = {
-    name: "hitesh",
-    email: "hitesh@lco.dev",
-    isActive: true
+// const User = {
+//     name: "hitesh",
+//     email: "hitesh@lco.dev",
+//     isActive: true
+// }
+
+// function createUser({ name: string, isPaid: boolean }) {
+
+// }
+
+// let newUser = { name: "hitesh", isPaid: false, email: "h@h.com" }
+
+// createUser(newUser)
+
+// function createCourse(): { name: string, price: number } {
+//     return { name: "reactjs", price: 399 }
+// }
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
 }
 
-function createUser({ name: string, isPaid: boolean }) {
-
+function createUser(user: User): User {
+    return { name: "", email: "", isActive: true }
 }
 
-let newUser = { name: "hitesh", isPaid: false, email: "h@h.com" }
-
-createUser(newUser)
-
-function createCourse(): { name: string, price: number } {
-    return { name: "reactjs", price: 399 }
-}
+createUser({ name: "", email: "", isActive: true })
 
 export { }
