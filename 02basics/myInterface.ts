f745dfc5d8ad7bf1f6284cc79badd61f8d5754db
enzoluizb <enzoluiz.brt@gmail.com>
2025-01-01 20:41:56 -0300

    interface vs type

--- 02basics/myInterface.ts
+++ 02basics/myInterface.ts
@@ -8,10 +8,20 @@ interface User {
     getCoupon(couponname: string, value: number): number
 }
 
-const hitesh: User = {
+interface User {
+    githubToken: string
+}
+
+interface Admin extends User {
+    role: "admin" | "ta" | "learner"
+}
+
+const hitesh: Admin = {
     dbId: 22,
     email: "h@h.com",
     userId: 2211,
+    role: "admin",
+    githubToken: "github",
     startTrail: () => {
         return "trail started"
     },
