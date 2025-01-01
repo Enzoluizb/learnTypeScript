e22a8f08a2bda94bf4dec4d21ee0ef6432d1c277
enzoluizb <enzoluiz.brt@gmail.com>
2025-01-01 19:48:36 -0300

    enums

---
+++ 02basics/myEnums.ts
@@ -0,0 +1,16 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+
+// if (seat === 0) {
+
+// }
+
+enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE = 3,
+    WINDOW,
+    FOURTH
+}
+
+const hcSeat = SeatChoice.AISLE
