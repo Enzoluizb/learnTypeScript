35b3444a1ce6843c1aa5ca12f8ca74a396fedcf3
enzoluizb <enzoluiz.brt@gmail.com>
2025-01-06 17:16:06 -0300

    classes

--- 03pureTS/src/index.ts
+++ 03pureTS/src/index.ts
@@ -1,2 +1,11 @@
-console.log("typescript is here")
-console.log("typescript is amazing")
+class User {
+    email: string
+    name: string
+    readonly city: string = "Jaipur"
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const asi = new User("a@a.com", "asi")
