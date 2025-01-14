996c66591a05bea666a5c19d25f84faafd14ff75
enzoluizb <enzoluiz.brt@gmail.com>
2025-01-14 10:08:26 -0300

    private / public

--- 03pureTS/src/index.ts
+++ 03pureTS/src/index.ts
@@ -1,11 +1,26 @@
+// class User {
+//     public email: string
+//     private name: string
+//     private readonly city: string = "Jaipur"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//         this.city
+//     }
+// }
+
+// const asi = new User("h@h.com", "haitech")
+// // asi.name
+
 class User {
-    email: string
-    name: string
-    readonly city: string = "Jaipur"
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
+
+    private readonly city: string = "Jaipur"
+    constructor(
+        public email: string,
+        public name: string,
+        // private userId: string
+    ) {
     }
 }
 
-// const asi = new User("h@h.com", "haitech")
+const asi = new User("h@h.com", "haitech")
