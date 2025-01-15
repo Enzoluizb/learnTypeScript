69ce8321c5bc61f6ee4cf254bfa48e20091c3ede
enzoluizb <enzoluiz.brt@gmail.com>
2025-01-15 10:09:06 -0300

    getters and setters

--- 03pureTS/src/index.ts
+++ 03pureTS/src/index.ts
@@ -14,6 +14,8 @@
 
 class User {
 
+    private _courseCount = 1
+
     private readonly city: string = "Jaipur"
     constructor(
         public email: string,
@@ -21,6 +23,26 @@ class User {
         // private userId: string
     ) {
     }
+    private deleteToken() {
+        console.log("Token deleted")
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
 }
 
 const asi = new User("h@h.com", "haitech")
+
+// asi.deleteToken()
