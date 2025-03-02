de9fcd880251eea2131eab234741ac4b68b5cded
enzoluizb <enzoluiz.brt@gmail.com>
2025-03-02 11:47:10 -0300

    first lesson

---
+++ otcurse/samurai1.ts
@@ -0,0 +1,16 @@
+// const name = 'Felipe'
+// const age = 25
+
+// '2' + 2
+// // resultado: 22
+
+const fullName = (user) => {
+    return `${user.firstName} ${user.lastName}`
+}
+
+const user = {
+    firstName: 'Enzo',
+    lastName: 'Luiz'
+}
+
+fullName(user)
